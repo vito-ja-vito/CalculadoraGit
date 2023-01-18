a48d98a2b240e5ec64655d74777389eceeaa118b
Javi <daw01.2023@gmail.com>
2023-01-18 09:47:19 +0100

    Funcion Suma

--- src/calculadoraGit/calculadoraGit.java
+++ src/calculadoraGit/calculadoraGit.java
@@ -1,5 +1,8 @@
 package calculadoraGit;
 
 public class calculadoraGit {
-
+	public int sumar(int a, int b) {
+		return a + b;
+		}
+		
 }
--- src/calculadoraGit/principal.java
+++ src/calculadoraGit/principal.java
@@ -4,6 +4,7 @@ public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		calculadora miCalc = new calculadora();
 
 	}
 
