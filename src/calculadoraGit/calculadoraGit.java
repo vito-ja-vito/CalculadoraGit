1f8f74f5f1429de9799c7802f42d464d4eb02fb4
Javi <daw01.2023@gmail.com>
2023-01-18 09:37:47 +0100

    proyectoBase

---
+++ src/calculadoraGit/calculadoraGit.java
@@ -0,0 +1,5 @@
+package calculadoraGit;
+
+public class calculadoraGit {
+
+}
---
+++ src/calculadoraGit/principal.java
@@ -0,0 +1,10 @@
+package calculadoraGit;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
