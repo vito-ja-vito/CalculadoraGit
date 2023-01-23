83ca8f816ce09e6497283f4cc1b36695ee90ff19
Javi <daw01.2023@gmail.com>
2023-01-23 12:14:05 +0100

    Función Multiplicar

---
+++ src/calculadoraGit/calculadora.java
@@ -0,0 +1,13 @@
+package calculadoraGit;
+
+public class calculadora {
+	public int sumar(int a, int b) {
+		return a + b;
+		}
+	public int restar(int a, int b) {
+		return a - b;
+		}
+	public int multiplicar(int a, int b) {
+		return a * b;
+		}
+}
